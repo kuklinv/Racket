c0f628cb5915946c3b9100c3c163829529f0297f
kuklinv <vitalykuklin@gmail.com>
2019-03-25 17:53:21 +0700

    about sum-of-squares with define & let

--- hexletBasic.rkt
+++ hexletBasic.rkt
@@ -4,9 +4,15 @@
 ;(display midl)
 ;(define (sum-of-squares x y)(+ (* x x) (* y y)))
 ;(sum-of-squares 2 3)
+
+;недоделанная реализация через let. нужно сделать
 (define (sum-of-squares x y)
   (let* ([sum (lambda (m n) (+ m n))])
    (* (sum x y) (sum x y))
   )
 )
 (sum-of-squares 2 3)
+; альтернативная рабочая реализация через define ниже
+(define (sum x y) (+ x y))
+(define (square-of-sum x y)
+          (* (sum x y) (sum x y)))
